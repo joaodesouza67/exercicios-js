87c47d3230e66d03514529e315f6bb4b33831b3c
João de Souza <j.a.desouza@hotmail.com>
2019-12-13 00:06:43 -0300

    Adicionado arquivo Booleanos.js

--- Fundamentos/TemplateString.js
+++ Fundamentos/TemplateString.js
@@ -3,4 +3,11 @@ const concatenacao = "Olá " + nome + " !";
 const template = `
     Olá
     ${nome} !`;
-console.log(template);
+console.log(concatenacao, template);
+
+//expressões...
+console.log(`1 + 1 = ${1+1}`);
+
+const up = texto => texto.toUpperCase();
+console.log(`Ei... ${up('cuidado')} !`);
+
